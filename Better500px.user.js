1a76a207005fab99745c6a967a569693a2e4fbeb
Nicolas Hoizey <nicolas@hoizey.com>
2011-12-06 23:33:24 +0100

    now works on Chrome ! \o/

--- Better500px.user.js
+++ Better500px.user.js
@@ -24,62 +24,85 @@
 //
 // ==UserScript==
 // @name          Better500px
-// @namespace     http://dev.gasteroprod.com/Better500px-UserScript/
+// @namespace     com.gasteroprod.dev.500px
 // @description   Enhances 500px.com
-// @version       1.0
+// @version       1.1
 // @include       http://500px.com/*
 // @include       http://*.500px.com/*
 // ==/UserScript==
 
-// 500px already uses jQuery
-var $ = unsafeWindow.jQuery;
+var better500px = function() {
+    var isConnected = false,
+        username = '';
 
-$("head").append(" \
-<style> \
-.photos .thumb .info .right { opacity: 100; } \
-.photos .thumb .fav, .search_result .search_result_photo .fav { position: absolute; top: 3px;right: 3px; width: 16px; height: 16px; overflow: hidden; } \
-.search_result_photo { position: relative; } \
-</style> \
-");
+    // 500px already uses jQuery
+    if (typeof unsafeWindow != 'undefined') {
+   		jQuery = unsafeWindow.jQuery;
+   	} else {
+   		$.noConflict();
+   	}
 
-// check if the user is connected and get his username
-var isConnected = false;
-var username = '';
-if ($('#menu_profile_login').length == 0) {
-    var isConnected = true;
-    var username = $('#menu_profile .username').attr('href').replace(/\//,'');
-}
+    jQuery("head").append(" \
+        <style> \
+        .photos .thumb .info .right { opacity: 100; } \
+        .photos .thumb .fav, .search_result .search_result_photo .fav { position: absolute; top: 3px;right: 3px; width: 16px; height: 16px; overflow: hidden; } \
+        .search_result_photo { position: relative; } \
+        </style> \
+        ");
 
-if (isConnected) {
+    // check if the user is connected and get his username
+    if (jQuery('#menu_profile_login').length == 0) {
+        var isConnected = true;
+        var username = jQuery('#menu_profile .username').attr('href').replace(/\//,'');
+    }
 
-    // check if this is a page with a list of photos
-    if ($('.photos').length > 0) {
+    if (isConnected) {
 
-        // check if this is not my own favorites page
-        var isOwnFavorites = new RegExp('^http://500px.com/' + username + '/favorites');
-        if (!isOwnFavorites.exec(location.href)) {
+        // check if this is a page with a list of photos
+        if (jQuery('.photos').length > 0) {
+
+            // check if this is not my own favorites page
+            var isOwnFavorites = new RegExp('^http://500px.com/' + username + '/favorites');
+            if (!isOwnFavorites.exec(location.href)) {
+                // show for each photo if it is already a favorite
+                jQuery('.thumb').each(function() {
+                    var that = jQuery(this);
+                    var url = that.find('a.image').attr('href');
+                    that.append('<div class="fav"></div>').find('.fav').load(url + ' #fave_link');
+                    that = null;
+                    url = null;
+                });
+            }
+        }
+
+        // check if this is a search result with a list of photos
+        if (jQuery('.search_result_photo').length > 0) {
+
             // show for each photo if it is already a favorite
-            $('.thumb').each(function() {
-                var that = $(this);
-                var url = that.find('a.image').attr('href');
+            jQuery('.search_result_photo').each(function() {
+                var that = jQuery(this);
+                var url = that.find('a').attr('href');
                 that.append('<div class="fav"></div>').find('.fav').load(url + ' #fave_link');
                 that = null;
                 url = null;
             });
         }
-    }
-    
-    // check if this is a search result with a list of photos
-    if ($('.search_result_photo').length > 0) {
 
-        // show for each photo if it is already a favorite
-        $('.search_result_photo').each(function() {
-            var that = $(this);
-            var url = that.find('a').attr('href');
-            that.append('<div class="fav"></div>').find('.fav').load(url + ' #fave_link');
-            that = null;
-            url = null;
-        });
     }
-    
+}
+
+// taken from @freeatnet_en's 500px_infscroll GM script
+if (navigator.userAgent.match(/Firefox/)) {
+	unsafeWindow.onload = better500px;
+} else if (navigator.userAgent.match(/Chrome/)) {
+	var script = document.createElement("script");
+	script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js");
+	script.addEventListener('load', function() {
+		var script = document.createElement("script");
+		script.textContent = "(" + better500px.toString() + ")();";
+		document.body.appendChild(script);
+	}, false);
+	document.body.appendChild(script);
+} else {
+	alert("I do not know what to do :(");
 }
