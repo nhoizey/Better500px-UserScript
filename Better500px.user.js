55e929af24c1e775dd6c899d1319ff042ccc3260
Nicolas Hoizey <nicolas@hoizey.com>
2012-01-16 15:19:08 +0100

    copyright continues

--- Better500px.user.js
+++ Better500px.user.js
@@ -1,4 +1,4 @@
-// Copyright (c) Nicolas Hoizey 2011
+// Copyright (c) Nicolas Hoizey 2011-2012
 // Released under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported licence (CC BY-NC-SA 3.0) 
 // http://creativecommons.org/licenses/by-nc-sa/3.0/
 //
